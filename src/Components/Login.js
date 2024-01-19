7793eb163d0696ecbd284c3f3da6b2d4d4df4cf6
Ajitchy <ajit28298@gmail.com>
2024-01-19 22:25:20 +0530

    Login Function Update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validationErrors, setValidationErrors] = useState({email:'', password:''});
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+
+  const validateForm = () => {
+    const errors = {};
+    if(!email){
+      errors.email = "Email is required";
+    }
+    if(!password){
+      errors.password = "Password is required";
+    }
+    return errors
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault(); //prevent the form from refreshing the page 
+    const errors = validateForm();
+    setValidationErrors(errors);
+    if(Object.keys(errors).length === 0){
+      await fetch(`http://localhost:8080/user?email=${email}&password=${password}`)
+      .then((response) => {
+        return response.json();
+      }).then((response) => {
+        if(Object.keys(response).length === 0){
+          toast.error('Invalid email or password');
+        }else {
+          if(response[0].password === password){
+            toast.success('Login Successful');
+            navigate('/')
+          }else{
+            toast.error('Invalid email or password');
+          }
+        }
+      }).catch((error) => {
+        toast.error('Error Occured:' +error.message);
+        setErrorMessage('An error occured');
+      });
+    }
+  };
+
   return (
     <>
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -12,7 +56,7 @@ export default function Login() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="/" method="POST">
+          <form className="space-y-6" noValidate onSubmit={handleLogin}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -20,12 +64,14 @@ export default function Login() {
               <div className="mt-2">
                 <input
                   id="email"
+                  value={email}
                   name="email"
                   type="email"
                   autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                  onChange={e => setEmail(e.target.value)}
+                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
                 />
+                <span className='text-red-500'>{validationErrors.email}</span>
               </div>
             </div>
 
@@ -43,12 +89,14 @@ export default function Login() {
               <div className="mt-2">
                 <input
                   id="password"
+                  value={password}
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                  onChange={e => setPassword(e.target.value)}
+                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
                 />
+                <span className='text-red-500'>{validationErrors.password}</span>
               </div>
             </div>
 
@@ -61,6 +109,7 @@ export default function Login() {
               </button>
             </div>
           </form>
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{' '}
